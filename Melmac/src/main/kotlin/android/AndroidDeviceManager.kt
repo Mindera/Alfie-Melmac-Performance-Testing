922ef282ffa7fe5879ff44b83715402df1cc0d4d
Vasco Sousa <vasco.sousa@mindera.com>
2025-05-30 14:38:20 +0100

    Removed acelleration from HVF

--- Melmac/src/main/kotlin/android/AndroidDeviceManager.kt
+++ Melmac/src/main/kotlin/android/AndroidDeviceManager.kt
@@ -36,6 +36,7 @@ object AndroidDeviceManager : DeviceManager {
                         "-no-audio",
                         "-gpu",
                         "swiftshader_indirect",
+                        "-accel", "off"
                 )
 
         val process = ProcessBuilder(args).redirectErrorStream(true).start()
